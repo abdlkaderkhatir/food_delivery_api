1972bf8e28b9073dcbcc09824c553ade63c1e4af
abdlkaderkhatir <a.khatir@esi-sba.dz>
2024-07-20 18:23:20 +0100

    sync Item Food

--- src/application/sync/SyncService.ts
+++ src/application/sync/SyncService.ts
@@ -1,26 +1,26 @@
-// import axios from 'axios'; // Import the 'axios' library
+import axios from 'axios'; // Import the 'axios' library
 
-// import { Food } from "../../domain/entities/Food";
-// import { FoodRepository } from "../../infrastructure/repositories/FoodRepository";
+import { Item } from "../../domain/entities/Item";
+import { ItemRepository } from "../../infrastructure/repositories/ItemRepository";
 
-// const syncWithDataServer = async () : Promise<void> => {
-//     try {
-//         const foodRepository = new FoodRepository();
-//         const { data: foods } = await axios.get<Food[]>('http://localhost:3000/sync');
+const syncWithDataServer = async () : Promise<void> => {
+    try {
+        const itemRepository = new ItemRepository();
+        const { data: foods } = await axios.get<Item[]>('http://localhost:3000/sync');
 
-//        // Example: Update local database with synchronized data
-//         await Promise.all(foods.map(async (food: Food) => {
-//             const existingFood = await foodRepository.findById(food.id);
-//             if (existingFood) {
-//             await foodRepository.update(food);
-//             } else {
-//             await foodRepository.create(food);
-//             }
-//         }));
-//     } catch (error : any) {
-//         console.log('Error syncing with data server: ', error.message);
-//     }
-// }
+       // Example: Update local database with synchronized data
+        await Promise.all(foods.map(async (food: Item) => {
+            const existingFood = await itemRepository.fetchItem(food._id);
+            if (existingFood) {
+            await itemRepository.updateItem(food._id ,food);
+            } else {
+            await itemRepository.createItem(food);
+            }
+        }));
+    } catch (error : any) {
+        console.log('Error syncing with data server: ', error.message);
+    }
+}
 
 
-// export default syncWithDataServer;
+export default syncWithDataServer;
--- src/interfaces/controllers/SyncController.ts
+++ src/interfaces/controllers/SyncController.ts
@@ -1,12 +1,12 @@
 // src/interfaces/controllers/SyncController.ts
 
 import { Request, Response } from 'express';
-// import syncWithDataServer from '../../application/sync/SyncService';
+import syncWithDataServer from '../../application/sync/SyncService';
 
 
 const synchronizeData = async (req: Request, res: Response): Promise<void> => {
   try {
-    // await syncWithDataServer();
+    await syncWithDataServer();
     res.json({ message: 'Data synchronization successful' });
   } catch (error) {
     res.status(500).json({ message: 'Failed to synchronize data' });
